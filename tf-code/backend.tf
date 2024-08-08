620ee93b4a27a05dd972715df1c0f4b259e84e1c
reddy1817 <reddy1817@gmail.com>
2024-08-08 16:06:43 +0100

    bucket name changed

--- tf-code/backend.tf
+++ tf-code/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     encrypt        = true
-    bucket         = "S3_BUCKET_NAME" #REPLACE WITH YOUR BUCKET
+    bucket         = "rgas-dev-red-sun" #REPLACE WITH YOUR BUCKET
     dynamodb_table = "dynamo-terraform-state-lock" #REPLACE WITH YOUR DYNAMO TABLE
     key            = "terraform.tfstate"
     region         = "us-east-1" #REPLACE WITH YOUR REGION
